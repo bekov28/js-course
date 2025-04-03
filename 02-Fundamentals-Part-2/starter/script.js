720b40b652f471660059ea6446c87bdeabe13308
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-03 23:09:29 +0900

    use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriverLicence = true;
+// if (hasDriversLicence) console.log("I can't drive");
+
+//Functions
+
+function logger() {
+  console.log("My name is Berd");
+}
+logger();
