ed64ee452e6eccbaef869ff802e372b096dec4f6
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-04 14:30:42 +0900

    function practice

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,7 +8,33 @@
 
 //Functions
 
-function logger() {
-  console.log("My name is Berd");
-}
-logger();
+// function logger() {
+//   console.log("My name is Berd");
+// }
+// logger();
+// logger();
+
+//Function with paratametrs
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+//Function declaration
+
+// function calcAge(birthYear) {
+//     return 2025 - birthYear;
+// }
+// const age = calcAge(1968);
+// console.log(age);
+
+//Function Expression
+
+const calcAge = function (birthYear) {
+  return 2025 - birthYear;
+};
+const age = calcAge(1968);
+console.log(age);
