91db057635eb74ddec7c146685058ef61549df52
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-05 22:40:22 +0900

    array practice

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -33,8 +33,41 @@
 
 //Function Expression
 
-const calcAge = function (birthYear) {
-  return 2025 - birthYear;
-};
-const age = calcAge(1968);
-console.log(age);
+// const calcAge = function (birthYear) {
+//   return 2025 - birthYear;
+// };
+// const age = calcAge(1968);
+// console.log(age);
+
+
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+ 
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+ 
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log('No team wins...');
+//   }
+// };
+ 
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
+// const calcTip = function(bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+// console.log(calcTip(100));
+
+// const bills = [125, 555, 44];
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
