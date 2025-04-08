07802aa684e5b4a2e0f58a2271141531218c9720
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-08 11:20:23 +0900

    loop breakdown

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -138,12 +138,12 @@
 //     console.log(`Hello World! ${rep}`)
 // }
 
+//Loop backwards
 
-const jonasArray = [
-    'Jonas',
-    'Schmidt',
-    2037-1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steven']
-]
+// const jonas = ["Jonas", "Schmidt", 2037 - 1991, "teacher", ["Michael", "Peter", "Steven"]];
+
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//   console.log(i, jonas[i]);
+// }
+
 
