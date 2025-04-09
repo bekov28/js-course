dcd457b3f11d63a5fe364e325ed4d2aa89e20c3d
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-09 23:53:18 +0900

    querySelector practice

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,7 @@
 'use strict';
 
+console.log(document.querySelector('.message').textContent);
 
+document.querySelector('.message').textContent = 'Correct Number👋';
+
+document.querySelector('.guess').value = 23;
