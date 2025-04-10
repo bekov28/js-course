c015fd5c1800f2efb000e4da17c9a77b1e232327
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-10 10:48:17 +0900

    event push

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,18 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number👋';
 
 document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+ if (!guess) {
+    document.querySelector('.message').textContent = 'No number!'
+ }
+});
