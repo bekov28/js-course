d07b74541cedfc25e15c08b7edd2ca3c3d91411c
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-11 14:55:54 +0900

    push

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -22,24 +22,25 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess == secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
-     if (score > 1) {
-        document.querySelector('.message').textContent = 'Too high!';
-        score --;
-        document.querySelector('.score').textContent = score;
-     } else {
-        document.querySelector('.message').textContent = 'You lost the game!';
-        document.querySelector('.score').textContent = 0;
-     }
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
     if (score > 1) {
-        document.querySelector('.message').textContent = 'Too low!';
-        score --;
-        document.querySelector('.score').textContent = score;
-     } else {
-        document.querySelector('.message').textContent = 'You lost the game!';
-        document.querySelector('.score').textContent = 0;
-     }
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
-
