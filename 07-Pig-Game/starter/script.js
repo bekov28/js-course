2f3a4b792a7aafd8b251a5609cfd8a2277913856
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-13 11:41:27 +0900

    push

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -20,6 +20,15 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 //Rolling dice
 
 btnRoll.addEventListener('click', function () {
@@ -39,11 +48,23 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     ///switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent =
-    0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+//Holding
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  console.log(scores[activePlayer]);
+  //  scores[1] = scores[1] + currentScore;
+
+  document.getElementById(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. Check if player's score is >=100
+  //Finish the game
+
+  //If not, switch to the next player
+  switchPlayer();
 });
