7f0212e2f74e54a98b8902ced352894531b405ed
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-16 06:15:02 +0900

    hoisting practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+console.log(me); //undefined
+console.log(job); //ReferenceError
+console.log(year); //ReferenceError
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//Functions
+
+console.log(addDecl(2, 3)); //5
+console.log(addEdExpr(2, 3)); //ReferenceError
+console.log(addArrow(2, 3)); //ReferenceError
+console.log(addAVarFunction(2, 3)); //TypeError -> undefined cant be function
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addEdExpr = function (a, b) {
+  return a + b;
+};
+const addArrow = (a, b) => a + b;
+
+var addAVarFunction = (a, b) => a + b; //error
+
