d93e380cfd97f9ed92a2a1394cc55801a186fe79
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-06-09 23:59:16 +0900

    array desctructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -46,5 +46,10 @@ const restaurant = {
   },
 };
 
-//console.log(3 || 'Jonas'); // 3
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
+const [x, y, z] = arr;
+console.log(x, y, z);
