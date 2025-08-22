b52ed9ac7f56396579241aa24a939bc80bd9d3f7
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-08-22 23:02:53 +0900

    Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -22,22 +22,34 @@
 // createBooking('LH123');
 // createBooking('LH123', 2, 800); //flightNum: 'LH123', numPassengers: 2, price: 800
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtman',
-  passport: 2343533654,
-};
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH99';
-  passenger.name = 'Mr.' + passenger.name;
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtman',
+//   passport: 2343533654,
+// };
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH99';
+//   passenger.name = 'Mr.' + passenger.name;
 
-  if (passenger.passport === 2343533654) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport');
-  }
-};
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+//   if (passenger.passport === 2343533654) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
 
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000);
+// };
+// newPassport(jonas);
+// checkIn(flight, jonas);
+// screen;
+
+//********First class and higher-order functions********
+//First class functions
+//1. JavaScript treats functions as first class citizens;
+//2. This means that functions are simply values;
+//3. Functions are just another 'type' of object;
