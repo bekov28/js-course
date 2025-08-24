4974b6d0fe11d5b27d3f33074e512b9374108347
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-08-24 18:27:30 +0900

    callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -73,3 +73,29 @@ function count() {
 
 //This function is returning another function, so it is also a higher order function
 //The difference between first-class functions and higher order function is that first class functions are just features or values
+
+//Functions accepting callback functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join('  ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
+const high5 = function () {
+  console.log('Hello World!');
+};
+
+document.body.addEventListener('click', high5); //high order function
