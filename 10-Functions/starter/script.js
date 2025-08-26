604fc12c81ae4da994ff2baa7e4a2b4b605e8ed6
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-08-26 23:49:28 +0900

    updated

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -159,18 +159,4 @@ book.apply(swiss, flightData);
 console.log(swiss);
 book.call(swiss, ...flightData);
 
-// function fizzBuzz(n) {
-//   // Write your code here
-//   for (let i = 1; i <= n; i++) {
-//     if (i % 3 === 0 && i % 5 === 0) {
-//       console.log('FizzBuzz');
-//     } else if (i % 3 === 0) {
-//       console.log('Fizz');
-//     } else if (i % 5 === 0) {
-//       console.log('Buzz');
-//     } else {
-//       console.log(i);
-//     }
-//   }
-// }
-// fizzBuzz(15);
+
